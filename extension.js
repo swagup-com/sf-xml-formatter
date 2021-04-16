31b4568090145f51b166f714d0f595b047828446
Dainel Rivero <drival1994@gmail.com>
2021-04-16 19:24:04 -0400

    Add option to enable/disable the formatter

--- extension.js
+++ extension.js
@@ -1,31 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const xml2js = require("xml2js");
 const fs = require("fs");
 const { sort } = require("./sorter.js");
+const {
+  getFormatSettings,
+  isFormatDisabled,
+} = require("./settings/settings.js");
 
 const encoding = "utf-8";
 const sortConfigurationFilePath = __dirname + "/xmlformatter.cfg";
 const packageJsonFilePath = __dirname + "/package.json";
 
+const formatSettings = getFormatSettings();
 const sortDefaultConfiguration = {
   relevantKeys: {},
   nonSortKeys: [],
 };
 
-const parserOptions = {
-  trim: true, // Trim the whitespace at the beginning and end of text nodes
-};
-
-const builderOptions = {
-  xmldec: { version: "1.0", encoding: "UTF-8", standalone: null },
-};
-
 const loadFileFromDisk = function (path) {
   let fileObj = undefined;
   try {
-    console.log("path_exists: " + fs.existsSync(path));
     if (fs.existsSync(path)) {
       fileObj = JSON.parse(
         fs.readFileSync(path, { encoding: encoding, flag: "r" })
@@ -39,14 +33,9 @@ const loadFileFromDisk = function (path) {
 
 const getSortConfiguration = function () {
   let options = loadFileFromDisk(sortConfigurationFilePath);
-  console.log("options: " + JSON.stringify(options));
 
   if (!options) {
-    console.log("Create");
     options = sortDefaultConfiguration;
-    // fs.writeFileSync(sortConfigurationFilePath, JSON.stringify(options), {
-    //   flag: "a",
-    // });
   }
 
   return options;
@@ -55,8 +44,12 @@ const getSortConfiguration = function () {
 vscode.languages.registerDocumentFormattingEditProvider("xml", {
   provideDocumentFormattingEdits(document) {
     console.log("FORMATTING...");
+    console.log("isFormatDisabled: " + isFormatDisabled());
+    if (isFormatDisabled()) {
+      return null;
+    }
     let xmlContent = document.getText();
-    var parser = new xml2js.Parser(parserOptions);
+    var parser = new xml2js.Parser(formatSettings.parserOptions);
     let sortedXml;
     let errorMsg = "";
 
@@ -64,14 +57,14 @@ vscode.languages.registerDocumentFormattingEditProvider("xml", {
       if (result) {
         try {
           console.log("SORTING...");
-          let builder = new xml2js.Builder(builderOptions);
+          let builder = new xml2js.Builder(formatSettings.builderOptions);
           const sortConfiguration = getSortConfiguration();
           let sortedJsonObj = sort(result, sortConfiguration);
           sortedXml = builder.buildObject(sortedJsonObj);
           console.log("END.");
         } catch (error) {
           errorMsg = "An unexpected error has occurred. Details: " + error;
-          console.log(errorMsg);
+          console.error(errorMsg);
         }
       }
     });
@@ -106,10 +99,6 @@ vscode.languages.registerDocumentFormattingEditProvider("xml", {
 function activate(context) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    '"sf-xml-formatter" is now active!\nlanguage: ' +
-      vscode.window.activeTextEditor.document.languageId
-  );
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
---
+++ settings/settings.js
@@ -0,0 +1,26 @@
+const vscode = require("vscode");
+
+const getFormatSettings = () => {
+  const defaultSettings = {
+    encoding: "utf-8",
+    parserOptions: {
+      trim: true, // Trim the whitespace at the beginning and end of text nodes
+    },
+    builderOptions: {
+      xmldec: { version: "1.0", encoding: "UTF-8", standalone: null },
+    },
+  };
+
+  return defaultSettings;
+};
+
+const isFormatDisabled = () => {
+  return vscode.workspace
+    .getConfiguration("sf-xml-formatter.format")
+    .get("enabled", false);
+};
+
+module.exports = {
+  getFormatSettings,
+  isFormatDisabled,
+};
